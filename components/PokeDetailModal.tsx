b3b4ab7d92e2c8dcc6c23d0065ad3135b88eb7ac
mike <namike623@gmail.com>
2022-09-16 00:21:12 +0100

    add modal and rating

--- components/PokeDetailModal.tsx
+++ components/PokeDetailModal.tsx
@@ -1,6 +1,15 @@
 import { Dialog, Transition } from "@headlessui/react";
+import Image from "next/image";
 import { Fragment, useState } from "react";
+import { useLocalStorage } from "../hooks/useLocalStorage";
 import { PokemonEnum, useGetPokemonByNameQuery } from "./pokemon-urql";
+import Star from "/public/assets/img/star.svg";
+
+type PokeMeta = {
+  rate: number;
+  comment: string;
+};
+type UserPokeMeta = Record<string, PokeMeta>;
 
 export default function MyModal({
   setIsOpen,
@@ -11,6 +20,10 @@ export default function MyModal({
   isOpen: boolean;
   pokemon?: PokemonEnum;
 }) {
+  const [userPokeMeta, setUserPokeMeta] = useLocalStorage<UserPokeMeta>(
+    "wedo:poke:meta",
+    {}
+  );
   const [result] = useGetPokemonByNameQuery({
     variables: { pokemon: pokemon as PokemonEnum },
     pause: !pokemon,
@@ -56,23 +69,51 @@ export default function MyModal({
                     as="h1"
                     className="text-2xl font-medium leading-6 text-gray-900"
                   >
-                    {pokeDetail?.key}
+                    {pokeDetail?.key || pokemon}
                   </Dialog.Title>
-                  <div className="mt-2">
-                    <p className="text-sm text-gray-500">
-                      {pokeDetail?.flavorTexts[0].flavor}
-                    </p>
-                  </div>
+                  {!result.fetching && pokeDetail && (
+                    <>
+                      <div className="mt-2">
+                        <p className="text-sm text-gray-500">
+                          {pokeDetail?.flavorTexts[0].flavor}
+                        </p>
+                      </div>
 
-                  <div className="mt-4">
-                    <button
-                      type="button"
-                      className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
-                      onClick={closeModal}
-                    >
-                      Got it, thanks!
-                    </button>
-                  </div>
+                      <div className="mt-4">
+                        <button
+                          type="button"
+                          className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                          onClick={closeModal}
+                        >
+                          Got it, thanks!
+                        </button>
+                      </div>
+
+                      <div className="flex items-center">
+                        {new Array(5).fill(0).map((_, idx) => {
+                          return (
+                            <Star
+                              key={idx}
+                              onClick={() =>
+                                setUserPokeMeta((d) => ({
+                                  ...d,
+                                  [pokeDetail.key]: {
+                                    ...d[pokeDetail.key],
+                                    rate: idx + 1,
+                                  },
+                                }))
+                              }
+                              className={`w-10 h-10 ${
+                                idx + 1 <= userPokeMeta?.[pokeDetail.key]?.rate
+                                  ? "text-yellow-400"
+                                  : "text-gray-300 dark:text-gray-500"
+                              }`}
+                            />
+                          );
+                        })}
+                      </div>
+                    </>
+                  )}
                 </Dialog.Panel>
               </Transition.Child>
             </div>
--- next.config.js
+++ next.config.js
@@ -1,13 +1,17 @@
 /** @type {import('next').NextConfig} */
-const WindiCSSWebpackPlugin = require('windicss-webpack-plugin')
+const WindiCSSWebpackPlugin = require("windicss-webpack-plugin");
 const nextConfig = {
   images: {
-    domains: ['play.pokemonshowdown.com'],
+    domains: ["play.pokemonshowdown.com"],
   },
   reactStrictMode: true,
   swcMinify: true,
   webpack: (config) => {
-    config.plugins.push(new WindiCSSWebpackPlugin())
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+    config.plugins.push(new WindiCSSWebpackPlugin());
     config.module.rules.push({
       test: /\.(graphql|gql)$/,
       exclude: /node_modules/,
