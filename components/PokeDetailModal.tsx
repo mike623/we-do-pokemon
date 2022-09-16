c5b51a6036dd271e875cc60ae0c9f539d3757326
mike <namike623@gmail.com>
2022-09-16 20:37:49 +0100

    update modal

--- components/PokeDetailModal.tsx
+++ components/PokeDetailModal.tsx
@@ -1,5 +1,5 @@
 import { Dialog, Transition } from "@headlessui/react";
-import { Fragment } from "react";
+import { Fragment, useEffect, useState } from "react";
 import { PokeDetail } from "./PokeDetail";
 import { PokemonEnum, useGetPokemonByNameQuery } from "./pokemon-urql";
 
@@ -12,6 +12,7 @@ export default function MyModal({
   isOpen: boolean;
   pokemon?: PokemonEnum;
 }) {
+  const [copied, setCopied] = useState(false);
   const [result] = useGetPokemonByNameQuery({
     variables: { pokemon: pokemon as PokemonEnum },
     pause: !pokemon,
@@ -24,6 +25,9 @@ export default function MyModal({
   function openModal() {
     setIsOpen(true);
   }
+  useEffect(() => {
+    setCopied(false);
+  }, [pokemon]);
 
   return (
     <>
@@ -55,9 +59,54 @@ export default function MyModal({
                 <Dialog.Panel className="w-full max-w-4xl transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
                   <Dialog.Title
                     as="h1"
-                    className="text-2xl font-medium leading-6 text-gray-900"
+                    className="text-2xl font-medium leading-6 text-gray-900 flex"
                   >
                     {pokeDetail?.key || pokemon}
+                    <div className="mx-3">
+                      <div
+                        role="button"
+                        className="flex"
+                        onClick={() => {
+                          setCopied(true);
+                          navigator.clipboard.writeText(
+                            location.origin + "/poke/" + pokemon
+                          );
+                        }}
+                      >
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke-width="1.5"
+                          stroke="currentColor"
+                          className="w-6 h-6"
+                        >
+                          <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            d="M7.217 10.907a2.25 2.25 0 100 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186l9.566-5.314m-9.566 7.5l9.566 5.314m0 0a2.25 2.25 0 103.935 2.186 2.25 2.25 0 00-3.935-2.186zm0-12.814a2.25 2.25 0 103.933-2.185 2.25 2.25 0 00-3.933 2.185z"
+                          />
+                        </svg>
+                      </div>
+                    </div>
+                    <div>
+                      {copied && (
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke-width="1.5"
+                          stroke="currentColor"
+                          className="w-6 h-6 ml-3 fill-orange-100"
+                        >
+                          <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                          />
+                        </svg>
+                      )}
+                    </div>
                   </Dialog.Title>
                   {!result.fetching && pokeDetail && (
                     <div>
