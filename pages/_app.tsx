b8cf35a47b9a3adf725d263ac5e4429f20d87f45
mike <namike623@gmail.com>
2022-09-18 15:17:11 +0100

    disable zoome

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,18 @@
 import "../styles/globals.css";
-import 'windi.css'
+import "windi.css";
 import type { AppProps } from "next/app";
 import { GqlProvider } from "../services/gql";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <GqlProvider>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
+      </Head>
       <Component {...pageProps} />
     </GqlProvider>
   );
